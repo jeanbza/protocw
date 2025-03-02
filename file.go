d37847e72b32cb62d9b4bb71446cda71c46efa59
Jean Barkhuysen <jbarkhuysen@netflix.com>
2025-03-02 10:02:46 -0700

    Mostly working, needs better replacement

--- file.go
+++ file.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func searchDirForProto(dir, proto string) (string, error) {
@@ -27,4 +29,29 @@ func searchDirForProto(dir, proto string) (string, error) {
 		return "", fmt.Errorf("found multiple protos named %s: %v", proto, results)
 	}
 	return results[0], nil
+}
+
+// Walks outDir, replacing oldImportRoot with newImportRoot in every .go file it
+// sees.
+func replaceImports(outDir, oldImportRoot, newImportRoot string) error {
+	return filepath.Walk(outDir, func(path string, info os.FileInfo, err error) error {
+		fmt.Println("Inspecting", path)
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".go") {
+			input, err := os.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			output := bytes.Replace(input, []byte(oldImportRoot), []byte(newImportRoot), -1)
+			if !bytes.Equal(input, output) {
+				if err = os.WriteFile(path, output, info.Mode().Perm()); err != nil {
+					return err
+				}
+				fmt.Printf("Replaced %s with %s in %s\n", oldImportRoot, newImportRoot, path)
+			}
+		}
+		return nil
+	})
 }
--- main.go
+++ main.go
@@ -1,54 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("protocw <config file>")
+	if len(os.Args) != 3 {
+		fmt.Println("protocw <config file> <out dir>")
 		os.Exit(1)
 	}
 	ctx := context.Background()
 	configFile := os.Args[1]
-	if err := run(ctx, configFile); err != nil {
+	outDir := os.Args[2]
+	if err := run(ctx, configFile, outDir); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
-func run(ctx context.Context, configFile string) error {
+func run(ctx context.Context, configFile, outDir string) error {
 	c, err := loadConfig(configFile)
 	if err != nil {
 		return fmt.Errorf("error loading config: %v", err)
 	}
 
-	dir, err := os.MkdirTemp("", "protocw")
+	tmpRoot, err := os.MkdirTemp("", "protocw")
 	if err != nil {
 		return err
 	}
 
 	var b protocBuilder
-
 	for _, d := range c.Includes {
-		if err := cloneInto(ctx, dir, d.Repo); err != nil {
-			return fmt.Errorf("error cloning %s into %s: %v", d.Repo, dir, err)
+		if err := cloneInto(ctx, tmpRoot, d.Repo); err != nil {
+			return fmt.Errorf("error cloning %s into %s: %v", d.Repo, tmpRoot, err)
 		}
 		for _, proto := range d.Protos {
-			path, err := searchDirForProto(dir, proto)
+			path, err := searchDirForProto(tmpRoot, proto)
 			if err != nil {
-				return fmt.Errorf("error searching for %s in %s: %v", proto, dir, err)
+				return fmt.Errorf("error searching for %s in %s: %v", proto, tmpRoot, err)
 			}
 			fmt.Printf("Found proto %s in %s at %s\n", proto, d.Repo, path)
-			b.addInclude(proto, path)
+			if err := b.addInclude(proto, path, tmpRoot); err != nil {
+				return err
+			}
 		}
 	}
 
-	cmd := b.build(ctx, "protogen")
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		return fmt.Errorf("error creating %s: %v", outDir, err)
+	}
+
+	cmd := b.build(ctx, outDir)
 	var o, e bytes.Buffer
 	cmd.Stdout = &o
 	cmd.Stderr = &e
@@ -56,27 +65,86 @@ func run(ctx context.Context, configFile string) error {
 		return fmt.Errorf("error running %s:\n%s%s: %v", cmd.Args, o.String(), e.String(), err)
 	}
 
+	mr, err := modRoot(ctx)
+	if err != nil {
+		return err
+	}
+	for _, i := range c.Includes {
+		oldImportRoot := i.Repo
+		newImportRoot := filepath.Join(mr, outDir)
+		fmt.Printf("replacing %s with %s in %s\n", oldImportRoot, newImportRoot, outDir)
+		if err := replaceImports(outDir, oldImportRoot, newImportRoot); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
-type protocBuilder struct {
-	includes map[string]string
+func modRoot(ctx context.Context) (string, error) {
+	cmd := exec.CommandContext(ctx, "go", "mod", "graph")
+	var o, e bytes.Buffer
+	cmd.Stdout = &o
+	cmd.Stderr = &e
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("error running %s:\n%s%s: %v", cmd.Args, o.String(), e.String(), err)
+	}
+	scanner := bufio.NewScanner(&o)
+	if scanner.Scan() {
+		t := scanner.Text()
+		s := strings.Split(t, " ")
+		if len(s) == 0 {
+			return "", fmt.Errorf("error running %s: output %s doesn't look like it contains a go module", cmd.Args, s)
+		}
+		return s[0], nil
+	}
+	if err := scanner.Err(); err != nil {
+		return "", fmt.Errorf("error running %s: %v", cmd.Args, err)
+	}
+	return "", fmt.Errorf("error running %s: got not output", cmd.Args)
 }
 
-func (p *protocBuilder) addInclude(proto, pathToProto string) {
-	if p.includes == nil {
-		p.includes = make(map[string]string)
+type protocTriplet struct {
+	// ex: buf/validate/validate.proto.
+	protoImportPath string
+	// The -I.
+	//
+	// ex: /path/to/protovalidate/proto/protovalidate.
+	includeDir string
+	// Where this gets placed in the protogen dir.
+	// Concretely: the path that goes after --go_opt=M<proto>=cosmoscommons/.
+	//
+	// ex: buf/validate (which translates to <out dir>/buf/validate).
+	outDir string
+}
+
+type protocBuilder struct {
+	includes []*protocTriplet
+}
+
+func (p *protocBuilder) addInclude(protoImportPath, includeDir, tmpRoot string) error {
+	outDir := strings.TrimPrefix(includeDir, tmpRoot)
+	if outDir == "" {
+		return fmt.Errorf("unable to determine a directory for generation of %s", protoImportPath)
 	}
-	p.includes[proto] = pathToProto
+	p.includes = append(p.includes, &protocTriplet{
+		protoImportPath: protoImportPath,
+		includeDir:      includeDir,
+		outDir:          outDir,
+	})
+	return nil
 }
 
 func (p *protocBuilder) build(ctx context.Context, outDir string) exec.Cmd {
 	cmd := exec.CommandContext(ctx, "protoc")
-	for proto := range p.includes {
-		cmd.Args = append(cmd.Args, proto)
+	for _, t := range p.includes {
+		cmd.Args = append(cmd.Args, t.protoImportPath)
 	}
-	for _, path := range p.includes {
-		cmd.Args = append(cmd.Args, "-I", path)
+	for _, t := range p.includes {
+		cmd.Args = append(cmd.Args, "-I", t.includeDir)
+	}
+	for _, t := range p.includes {
+		cmd.Args = append(cmd.Args, fmt.Sprintf("--go_opt=M%s=%s", t.protoImportPath, t.outDir))
 	}
 	cmd.Args = append(cmd.Args, fmt.Sprintf("--go_out=:%s", outDir))
 	return *cmd
