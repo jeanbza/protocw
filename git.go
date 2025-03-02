897b5173d0c912fbbe050b80198ec5351284c01d
Jean Barkhuysen <jean.barkhuysen@gmail.com>
2025-03-02 11:33:13 -0700

    Small refactors

--- git.go
+++ git.go
@@ -50,3 +50,26 @@ func removeGoPackageOptions(dir string) error {
 		return nil
 	})
 }
+
+func searchDirForProto(dir, proto string) (string, error) {
+	var results []string
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if _, err := os.Lstat(filepath.Join(path, proto)); err == nil {
+			results = append(results, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if len(results) == 0 {
+		return "", os.ErrNotExist
+	}
+	if len(results) > 1 {
+		return "", fmt.Errorf("found multiple protos named %s: %v", proto, results)
+	}
+	return results[0], nil
+}
